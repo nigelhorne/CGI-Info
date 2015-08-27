70954e7fd5ebec164b928088ba3285093355cb37
Nigel Horne <njh@bandsman.co.uk>
2015-08-27 12:44:06 -0400

    Facebookexternal is a robot

--- lib/CGI/Info.pm
+++ lib/CGI/Info.pm
@@ -1187,7 +1187,7 @@ sub is_robot {
 		return 0;
 	}
 
-	if($agent =~ /.+bot|msnptc|is_archiver|backstreet|spider|scoutjet|gingersoftware|heritrix|dodnetdotcom|yandex|nutch|ezooms|plukkie|nova\.6scan\.com|Twitterbot/i) {
+	if($agent =~ /.+bot|msnptc|is_archiver|backstreet|spider|scoutjet|gingersoftware|heritrix|dodnetdotcom|yandex|nutch|ezooms|plukkie|nova\.6scan\.com|Twitterbot|facebookexternal/i) {
 		$self->{_is_robot} = 1;
 		return 1;
 	}
