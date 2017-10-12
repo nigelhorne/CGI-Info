dbf07d5d27c418d3ceab520c46d33ba14ccbb581
Nigel Horne <njh@bandsman.co.uk>
2017-10-12 13:16:19 -0400

    Send 405 on unsupported methods

--- lib/CGI/Info.pm
+++ lib/CGI/Info.pm
@@ -652,10 +652,12 @@ sub params {
 				warning => "POST: Invalid or unsupported content type: $content_type: $buffer",
 			});
 		}
-	# } elsif($ENV{'REQUEST_METHOD'} eq 'OPTIONS') {
-		# return;
-	# } elsif($ENV{'REQUEST_METHOD'} eq 'DELETE') {
-		# return;
+	} elsif($ENV{'REQUEST_METHOD'} eq 'OPTIONS') {
+		$self->{_status} = 405;
+		return;
+	} elsif($ENV{'REQUEST_METHOD'} eq 'DELETE') {
+		$self->{_status} = 405;
+		return;
 	} else {
 		# TODO: Design a way to tell the caller to send HTTP
 		# status 501
