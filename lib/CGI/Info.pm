1aa9542d25cfbf0cf80d016761945383600be973
Nigel Horne <njh@bandsman.co.uk>
2023-08-20 15:04:26 -0700

    Block more SQL injection attempts

--- lib/CGI/Info.pm
+++ lib/CGI/Info.pm
@@ -1348,7 +1348,7 @@ sub is_robot {
 		return 0;
 	}
 
-	if(($agent =~ /SELECT.+AND.+/) || ($agent =~ /ORDER BY /)) {
+	if(($agent =~ /SELECT.+AND.+/) || ($agent =~ /ORDER BY /) || ($agent =~ / OR NOT /) || ($agent =~ / AND \d+=\d+/)) {
 		$self->status(403);
 		$self->{is_robot} = 1;
 		if($self->{logger}) {
