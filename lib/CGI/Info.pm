41249b4fc89fe82ca7dfc8ad4de51305f3762d36
Nigel Horne <njh@bandsman.co.uk>
2025-08-07 15:45:46 -0400

    Fix copynpasta

--- lib/CGI/Info.pm
+++ lib/CGI/Info.pm
@@ -2123,7 +2123,7 @@ sub AUTOLOAD
 	# Ensure the method is called on the correct package object or a subclass
 	return unless((ref($self) eq __PACKAGE__) || (UNIVERSAL::isa((caller)[0], __PACKAGE__)));
 
-	# Validate method name - only allow safe config methods
+	# Validate method name - only allow safe parameter names
 	Carp::croak(__PACKAGE__, ": Invalid method name: $method") unless $method =~ /^[a-zA-Z_][a-zA-Z0-9_]*$/;
 
 	# Delegate to the param method
